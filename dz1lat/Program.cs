eff44eb8ec0bf71a8666ca63d8647487259be1da
Khammatov Karim <kupachel@gmail.com>
2026-09-06 14:48:01 +0300

    Сделал 4 задачи

--- dz1lat/Program.cs
+++ dz1lat/Program.cs
@@ -10,7 +10,44 @@ namespace dz1lat
     {
         static void Main(string[] args)
         {
+            //Задание 1
 
+
+            //Console.WriteLine($"Число e с точностью до десятых равно: {Math.Round(Math.E, 1)}");
+
+
+            //Console.ReadKey();
+
+
+            //Задание 2
+
+            //Console.WriteLine(50);
+            //Console.WriteLine(10);
+
+
+            //Console.ReadKey();
+
+
+            //Задание 3
+
+            //Random random1 = new Random();
+
+            //Console.WriteLine(random1.Next());
+            //Console.WriteLine(random1.Next());
+            //Console.WriteLine(random1.Next());
+            //Console.WriteLine(random1.Next());
+
+            //Console.ReadKey();
+
+            //Задание 4
+
+            //Console.WriteLine("Введите число");
+            //double number = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{number+10} больше введенного вами на 10");
+
+            //Console.ReadKey();
+
+            //Задание 5
         }
     }
 }
