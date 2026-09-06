8fadca9908a280d7d3d14295cd6bbae6e982ccf0
Khammatov Karim <kupachel@gmail.com>
2026-09-06 13:57:41 +0300

    Сделал все задания в учебнике Тумакова

--- dz1tum/Program.cs
+++ dz1tum/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,16 +12,86 @@ namespace dz1tum
     {
         static void Main(string[] args)
         {
-            //Задание 1
+            //Лаб.Задание 2.1
 
 
-            Console.WriteLine("Введите свое имя");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Здравствуйте {name}");
+            //Console.WriteLine("Введите свое имя");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Здравствуйте {name}");
+
+            //Console.ReadKey();
+
+
+            // Лаб.Задание 2.2
+
+            //Console.WriteLine("Введите делимое");
+            //int delimoe = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите делитель, не равный нулю");
+            //int delitel = Convert.ToInt32(Console.ReadLine());
+            //if (delitel == 0)
+            //{
+            //    Console.WriteLine("Вы ввели ноль, как делитель");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Частное равно {delimoe/delitel}");
+            //}
+
+            //Console.ReadKey();
+
+
+
+
+
+            //Дом задание 2.1
+
+            //Console.WriteLine("Введите латинскую букву, чтобы получить следующую в алфавите");
+            //char bukva = Convert.ToChar(Console.ReadLine());
+
+            //if ((bukva == 'z') || (bukva == 'Z'))
+            //{
+            //    Console.WriteLine("Это последняя буква в алфавите");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Следующая буква в алфавите : {Convert.ToChar(bukva + 1)}");
+            //}
+
+            //Console.ReadKey();
+
+            //Дом задание 2.2
+
+            Console.WriteLine("Ведите коэффицент a");
+
+            float a = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите коэффицент b");
+
+            float b = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите коэффицент c");
+
+            float c = float.Parse(Console.ReadLine());
+
+            float d = b*b - 4 * a * c;
+
+            if (d>0){
+                Console.WriteLine($"Первый x равен{(-b-(Math.Sqrt(d)))/(2*a)}, второй  x равен {(-b+(Math.Sqrt(d)))/(2*a)}");
+
+
+            }
+            else if (d==0){
+                Console.WriteLine($"X равен {(-b)/(2*a)}");
+            }
+            else
+            {
+                Console.WriteLine("Дискриминант меньше нуля, нет решения");
+            }
+
 
             Console.ReadKey();
 
 
-          }
+        }
     }
 }
